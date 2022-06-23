1a98b303a93d2a66c50fb356e1537cbaa50f8893
haspork <kondratev.pach@mail.ru>
2022-06-24 00:37:29 +0300

    a one artist and one track added to bd

--- backend/track/serializers.py
+++ backend/track/serializers.py
@@ -6,7 +6,7 @@ class ArtistSerializerForTrack(serializers.ModelSerializer):
     class Meta:
         model = Artist
         # fields = '__all__'
-        fields = ['nickname', 'birth_date']
+        fields = ['id']
 
 class TrackSerializer(serializers.ModelSerializer):
     artists_data = ArtistSerializerForTrack(source='artist', many=True)
