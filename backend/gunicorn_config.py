08ba5a51f7f8dc0ccf16e6d39d7f35f98f61fdfc
haspork <kondratev.pach@mail.ru>
2022-07-04 22:22:21 +0300

    namefixes

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/home/std/django-course-work/backend/venv/bin/gunicorn'
+command = '/home/std/django-course-work/backend/.venv/bin/gunicorn'
 pythonpath = '/home/std/django-course-work/backend'
 bind="127.0.0.1:8000"
 workers = 3
